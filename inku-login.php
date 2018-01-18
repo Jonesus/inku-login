f08647a185a2f36f065f0debd98df3bb6aafaa65
Jonesus <joonas.palosuo@gmail.com>
2018-01-18 18:47:44 +0200

    Update authors

--- inku-login.php
+++ inku-login.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Inku Login
  * Description:       A plugin that replaces the WordPress login flow with a custom page.
  * Version:           1.0.0
- * Author:            Jarkko Laine
+ * Author:            Jarkko Laine & Joonas Palosuo
  * License:           GPL-2.0+
  * Text Domain:       inku-login
  * Domain Path:				/languages
